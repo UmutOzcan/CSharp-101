098004230b033740835bf3be6f1e42d3eca8afba
Umut Ozcan <umut.yozcan@gmail.com>
2023-10-16 16:16:27 +0300

    if-else added

---
+++ if/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+int time = DateTime.Now.Hour;
+
+if (time >= 6 && time < 11)
+{
+    Console.WriteLine("Günaydın !");
+}
+else if (time <= 18)
+{
+    Console.WriteLine("Iyi günler !");
+}
+else
+{
+    Console.WriteLine("Iyi geceler !");
+}
+
+Console.WriteLine("\n" + "------------ Another Way -------------" + "\n");
+
+string sonuc = time >= 6 && time < 11 ? "Günaydın !" : time <= 18 ? "Iyi günler !" : "Iyi geceler!";
+Console.WriteLine(sonuc);
+
+Console.ReadKey();
