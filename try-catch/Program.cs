a793f5a5c5d53871fafea6147d99eab1b3d184ee
Umut Ozcan <umut.yozcan@gmail.com>
2023-10-16 15:57:15 +0300

    try-catch added

---
+++ try-catch/Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+try
+{
+    Console.Write("Sayi giriniz : ");
+    int sayi = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Girilen sayi : " + sayi);
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Hata : " + ex.Message.ToString());
+}
+finally
+{
+    Console.WriteLine("İslem tamamlandı.");
+}
+
+Console.WriteLine("\n" + "--------------------------------------------------------" + "\n");
+
+try
+{
+    int a = int.Parse(null);
+}
+catch (ArgumentNullException ex)
+{
+    Console.WriteLine("Hata : " + ex.Message.ToString());
+}
+
+
+Console.WriteLine("\n" + "--------------------------------------------------------" + "\n");
+
+try
+{
+    int b = int.Parse("test");
+}
+catch (FormatException ex)
+{
+    Console.WriteLine("Hata : " + ex.Message.ToString());
+}
+
+Console.WriteLine("\n" + "--------------------------------------------------------" + "\n");
+
+try
+{
+    int c = int.Parse("-20000000000000000000000");
+}
+catch (OverflowException ex)
+{
+    Console.WriteLine("Hata : " + ex.Message.ToString());
+}
+
+Console.ReadKey();
