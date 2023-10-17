83d8dae6f88ad2d7448ee1a5199c1cbb00301193
Umut Ozcan <umut.yozcan@gmail.com>
2023-10-17 22:09:31 +0300

    Array Methods added

---
+++ arrays2/Program.cs
@@ -0,0 +1,39 @@
+﻿int[] sayiDizisi = { 23, 12, 4, 86, 72, 3, 11, 17 };
+
+Console.WriteLine("*** Sirasiz Dizi ***");
+foreach (var sayi in sayiDizisi)
+    Console.WriteLine(sayi);
+
+//Sort
+Console.WriteLine("\n*** Sirali Dizi ***");
+Array.Sort(sayiDizisi);
+foreach (var sayi in sayiDizisi)
+    Console.WriteLine(sayi);
+
+//Clear
+Console.WriteLine("\n** Array Clear **");
+// sayiDizisi elemanlarini kullanarak 2.index ten itibaren 3 tane elemani 0'lar.
+Array.Clear(sayiDizisi, 2, 3);
+foreach (var sayi in sayiDizisi)
+    Console.WriteLine(sayi);
+
+//Reverse
+Console.WriteLine("\n** Array Reverse **");
+Array.Reverse(sayiDizisi);
+foreach (var sayi in sayiDizisi)
+    Console.WriteLine(sayi);
+
+//Index0f
+Console.WriteLine("\n** Array IndexOf **");
+Console.WriteLine(Array.IndexOf(sayiDizisi, 23));
+
+//Resize
+Console.WriteLine("\n** Array Resize **");
+Array.Resize<int>(ref sayiDizisi, 10);
+sayiDizisi[8] = 33;
+//sayiDizisi[9] = 66;
+foreach (var sayi in sayiDizisi)
+    Console.WriteLine(sayi);
+
+
+Console.ReadKey();
