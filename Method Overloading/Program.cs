fff85a757088819f477b0ba4d5b5bf216db98041
Umut Ozcan <umut.yozcan@gmail.com>
2023-10-23 21:41:08 +0300

    Method Overloading added

---
+++ Method Overloading/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Methods method = new();
+method.EkranaYazdir("1 Degisken iceren string veri tipi");
+method.EkranaYazdir(2);
+method.EkranaYazdir("2 Degisken iceren " + "string veri tipi");
+
+Console.ReadKey();
+
+class Methods
+{
+    public void EkranaYazdir(string veri)
+    {
+        Console.WriteLine(veri);
+    }
+
+    public void EkranaYazdir(int veri)
+    {
+        Console.WriteLine(veri);
+    }
+
+    public void EkranaYazdir(string veri1, string veri2)
+    {
+        Console.WriteLine(veri1 + veri2);
+    }
+
+}
+
