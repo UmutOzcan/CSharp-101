d74c6f164fb232413ed6ff771bc438c71a736217
Umut Ozcan <umut.yozcan@gmail.com>
2023-10-23 22:18:31 +0300

    Extension Methods added

---
+++ Extension Method/Program.cs
@@ -0,0 +1,22 @@
+﻿
+char[] dizi = "Merhaba".ConvertToChars();
+
+foreach (var item in dizi)
+{
+    Console.WriteLine(item);
+}
+
+Console.ReadKey();
+
+public static class Extensions
+{
+    public static char[] ConvertToChars(this string text)
+    {
+        char[] chars = new char[text.Length];
+        for (int i = 0; i < chars.Length; i++)
+        {
+            chars[i] = text[i];
+        }
+        return chars;
+    }
+}
