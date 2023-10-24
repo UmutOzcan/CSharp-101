4678f0b1fec3551bc400525aa0d4f4427d251a77
Umut Ozcan <umut.yozcan@gmail.com>
2023-10-24 22:59:59 +0300

    String Methods added

---
+++ String Methods/Program.cs
@@ -0,0 +1,28 @@
+﻿
+string text = "Hi guys !!";
+string text2 = "Hi";
+
+// Length
+Console.WriteLine("String uzunlugu => " + text.Length);
+
+//ToUpper - ToLower
+Console.WriteLine("String Uppercase =>" + text.ToUpper());
+Console.WriteLine("String Lowercase =>" + text.ToLower());
+
+//Concat
+Console.WriteLine("Stringi Whats Up ! ile birlestir =>" + String.Concat(text, " Whats Up !"));
+
+//Compare
+Console.WriteLine("String text2 den uzun mu =>" + text.CompareTo(text2)); // text > text2 -> 1 | text < text2 -> -1 | text = text2 -> 0
+
+//Contains
+Console.WriteLine("String text2 icerir mi =>" + text.Contains(text2));
+Console.WriteLine("String !! ile biter mi =>" + text.EndsWith("!!"));
+
+//Index Of
+Console.WriteLine("String guys kelimesini icerir mi =>" + text.IndexOf("guys"));
+
+//Split
+Console.WriteLine("String Splitin 1.elemani =>" + text.Split(" ")[0]);
+
+Console.ReadKey();
