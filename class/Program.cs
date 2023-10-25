f8f2322006833e7b713b729ca8fc1a10cd139014
Umut Ozcan <umut.yozcan@gmail.com>
2023-10-25 17:54:50 +0300

    class added

---
+++ class/Program.cs
@@ -0,0 +1,32 @@
+﻿
+Calisan calisan = new()
+{
+    Ad = "Umut",
+    Soyad = "Ozcan",
+    No = 5555555555,
+    Departman = "Development"
+};
+
+calisan.CalisanBilgileri();
+Console.ReadKey();
+
+
+class Calisan
+{
+    public string Ad;
+
+    public string Soyad;
+
+    public long No;
+
+    public string Departman;
+
+    public void CalisanBilgileri()
+    {
+        Console.WriteLine("Calisan Adi : {0}", Ad);
+        Console.WriteLine("Calisan Soyadi : {0}", Soyad);
+        Console.WriteLine("Calisan No : {0}", No);
+        Console.WriteLine("Calisan Departmani : {0}", Departman);
+    }
+
+}
