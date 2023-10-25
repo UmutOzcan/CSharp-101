3160aeba8c52390d49a4f960200b104a6395cb29
Umut Ozcan <umut.yozcan@gmail.com>
2023-10-25 18:06:45 +0300

    class update

--- class/Program.cs
+++ class/Program.cs
@@ -1,25 +1,41 @@
 ﻿
-Calisan calisan = new()
+Calisan calisan1 = new()
 {
     Ad = "Umut",
     Soyad = "Ozcan",
-    No = 5555555555,
-    Departman = "Development"
+    No = 55555555555,
+    Departman = "Lojistik"
 };
+calisan1.CalisanBilgileri();
 
-calisan.CalisanBilgileri();
+Calisan calisan2 = new("Yusuf", "Ozcan", 444444444, "Yazılım");
+calisan2.CalisanBilgileri();
+
 Console.ReadKey();
 
 
 class Calisan
 {
-    public string Ad;
+    public string? Ad;
 
-    public string Soyad;
+    public string? Soyad;
 
     public long No;
 
-    public string Departman;
+    public string? Departman;
+
+    public Calisan()
+    {
+    }
+
+    public Calisan(string Ad, string Soyad, long No, string Departman)
+    {
+        this.Ad = Ad;
+        this.Soyad = Soyad;
+        this.No = No;
+        this.Departman = Departman;
+
+    }
 
     public void CalisanBilgileri()
     {
@@ -27,6 +43,7 @@ class Calisan
         Console.WriteLine("Calisan Soyadi : {0}", Soyad);
         Console.WriteLine("Calisan No : {0}", No);
         Console.WriteLine("Calisan Departmani : {0}", Departman);
+        Console.WriteLine("\n");
     }
 
 }
