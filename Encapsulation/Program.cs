00200a99f55ffde5f179ebc2b1c3806862913273
Umut Ozcan <umut.yozcan@gmail.com>
2023-10-27 17:21:43 +0300

    Encapsulation & static class added

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+
+Student student = new();
+student.Name = "Umut";
+student.Surname = "Ozcan";
+student.No = 800;
+
+student.GetStudentInfo();
+Console.ReadKey();
+
+
+
+
+class Student
+{
+    private string name;
+    private string surname;
+    private int no;
+
+    public string Name { get => name; set => name = value; }
+
+    public string Surname { get => surname; set => surname = value; }
+
+    public int No { get => no; set => no = value; }
+
+    public Student() { }
+
+    public Student(string name, string surname, int no)
+    {
+        Name = name;
+        Surname = surname;
+        No = no;
+    }
+
+    public void GetStudentInfo()
+    {
+        Console.WriteLine(this.Name);
+        Console.WriteLine(this.Surname);
+        Console.WriteLine(this.No);
+
+    }
+
+
+}
---
+++ static class/Program.cs
@@ -0,0 +1,30 @@
+﻿//Static sınıf üyesine erişim
+Console.WriteLine("Öğrenci sayısı: {0}", Ogrenci.OgrenciSayisi);
+
+//Static olmayan sinif üyesine erişim
+Ogrenci ogrenci1 = new()
+{
+    Isim = "Deniz",
+    Soyisim = "Arda"
+};
+
+Ogrenci ogrenci2 = new()
+{
+    Isim = "Ayşe",
+    Soyisim = "Yılmaz"
+};
+
+Console.WriteLine("Öğrenci Sayısı: {0}", Ogrenci.OgrenciSayisi);
+
+Console.ReadKey();
+
+class Ogrenci
+{
+    public static int OgrenciSayisi = 0;
+    public string Isim;
+    public string Soyisim;
+    public Ogrenci()
+    {
+        OgrenciSayisi++;
+    }
+}
