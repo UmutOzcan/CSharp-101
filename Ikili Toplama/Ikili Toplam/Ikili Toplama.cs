97ba60fb679170038a6c27bc495f587ba1e611b4
Umut Ozcan <umut.yozcan@gmail.com>
2024-01-27 17:53:46 +0300

    [Add] Algortihm added

---
+++ Ikili Toplama/Ikili Toplam/Ikili Toplama.cs
@@ -0,0 +1,34 @@
+﻿public static class Toplama
+{
+    // Ekrandan girilen n tane integer ikililerin toplamını alan, eğer sayılar birbirinden farklıysa toplamlarını ekrana yazdıran,
+    // sayılar aynıysa toplamının karesini ekrana yazdıran fonksiyon
+    public static List<int> IkiliToplama(string input)
+    {
+        string[] dizi = input.Split(" ");
+        List<int> sums = [];
+
+        for (int i = 0; i < dizi.Length; i += 2)
+        {
+            if (int.TryParse(dizi[i], out int firstNumber) && int.TryParse(dizi[i + 1], out int secondNumber))
+            {
+                int sum = firstNumber + secondNumber;
+
+                if (firstNumber != secondNumber)
+                {
+                    sums.Add(sum);
+                }
+                else
+                {
+                    int square = sum * sum;
+                    sums.Add(square);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Hata: {dizi[i]} veya {dizi[i + 1]} geçerli bir sayı değil.");
+            }
+        }
+
+        return sums;
+    }
+}
---
+++ Ikili Toplama/Ikili Toplam/Main.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Integer ikilileri girin (boşlukla ayırarak):");
+string? input = Console.ReadLine();
+
+List<int> result = Toplama.IkiliToplama(input);
+
+Console.WriteLine("Toplamlar:");
+foreach (int sum in result)
+{
+    Console.Write(sum + " ");
+}
+
+Console.ReadKey();
